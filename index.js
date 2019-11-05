37d87d1fe22538c6b83fe34b020ffc5ccad595e2
Cibelle Montor <montorcibelle@gmail.com>
2019-11-04 21:34:47 -0700

    file started  installed packages

---
+++ index.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+        type = "input",
+        name = "color",
+        message = "What is your favorite color?"
+    }
+    ,
+    {
+        type = "input",
+        name = "github",
+        message = "What is your Github user?",
+    }
+
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() { }
+
+init() 
